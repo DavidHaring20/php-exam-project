4030b8669f6987a99e0dd63e2d0cbc2d650862d9
DavidHaring20 <davidharingri@gmail.com>
2022-11-21 20:24:38 +0100

    Added singup in api and modified .htaccess file. Added 404 error page.

---
+++ api/create-user.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once '../services/validator.php';
+require_once '../models/User.php';
+
+$first_name = validate_first_name();
+$last_name = validate_last_name();
+$email = validate_email();
+$username = validate_username();
+$password = validate_password();
+$password_retype = $_POST['passwordRetype'];
+
+if ($password !== $password_retype) {
+    echo $password;
+    echo $password_retype;
+    respond("Passwords do not match", 400);
+}
+
+$user = new User($first_name, $last_name, $email, $username, $password);
+echo $user;
+
+
+?>
